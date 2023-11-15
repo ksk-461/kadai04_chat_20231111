4a59e59eecc3d31db36b920c979304fa03b0fb8c
ksk-461 <lpcaster@yahoo.co.jp>
2023-11-16 00:08:18 +0900

    表示領域の整備

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-$(() =>{
-    $(".inp_txt").focus(() => {
-        $(".inp_txt").attr("placeholder", "メッセージを入力");
-    }).blur(() => {
-        $(".inp_txt").attr("placeholder", "Aa")
+const container = document.querySelector(".container");
+for(var i = 0; i <= 50; i++){
+    const blocks = document.createElement("div");
+    blocks.classList.add("block");
+    container.appendChild(blocks);
+}
+
+function animateBlocks() {
+    anime({
+        targets: ".block",
+        translateX: function(){
+            return anime.random(-500, 500);
+        },
+        translateY: function(){
+            return anime.random(-250, 250)
+        },
+        scale: function(){
+            return anime.random(2, 3)
+        },
+        duration: 7000,
+        delay: anime.stagger(15),
+        complete: animateBlocks,
     });
-});
+}
+
+animateBlocks();
