e25b1e453a4e5ed5b1e76470f0483602674298e1
ksk-461 <lpcaster@yahoo.co.jp>
2023-11-23 04:13:26 +0900

    fire.js修正

--- js/fire.js
+++ js/fire.js
@@ -2,11 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebas
 import { getDatabase, ref, push, set, onChildAdded, remove, onChildRemoved } 
 from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
 
-<<<<<<< HEAD
 const firebaseConfig = A_key;
-=======
-const firebaseConfig = A_Key;
->>>>>>> 587e721e01391407df248472fd4a1b3aab8cebcc
 const app = initializeApp(firebaseConfig);
 const db  = getDatabase(app);
 const dbRef = ref(db, "chat");
@@ -23,11 +19,46 @@ $("#submit").on("click", () => {
     }
 });
 
+$("#text").on("keydown",function(e){
+    if(e.keyCode == 13){
+        const msg = {
+            text : $("#text").val()
+        }
+        const newPostRef = push(dbRef);
+        set(newPostRef,msg);
+        $("#text").val(""); 
+    }
+});
+
+
 onChildAdded(dbRef, (data) =>{
     const msg = data.val();
     const key = data.key;
-    let h = '<div class="block">';
+    let h = '<div class="block" id="' + key + '">';
         h += msg.text;
         h += "</div>";
         $(".container").append(h);
 });
+
+$(".container").on("click",".block", function(){
+    const key = $(this).attr("id");
+    const remove_item = ref(db,"chat/" + key);
+    $(this).fadeOut(600, function(){remove(remove_item)});
+});
+
+onChildRemoved(dbRef, (data) => {
+    $("#"+data.key).remove();
+});
+
+/*
+memo:appendした内容はhoverではできないので、mouseenterで代替
+this以外にe.currentTargetも使える
+ブルブル小刻みに震えて欲しかったが断念
+$(".container").on("mouseenter",".block",function(e){
+    const key = $(this).attr("id");
+    console.log(key);
+    console.log(e.currentTarget)
+    $(e.currentTarget).css("background-color","red")
+    $(this).css("background-color","red");
+});
+*/
